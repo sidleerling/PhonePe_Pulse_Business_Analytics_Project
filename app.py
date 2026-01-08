ed1a4012b02b37e5cc3cc5b9ea9fa6aaaab6ac9f
Siddhant Iyer <siddhantiyer46@gmail.com>
2026-01-08 21:50:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -580,7 +580,7 @@ def load_business_figures():
                     title = "Top 5 Pincodes With Highest Growth in Insurance Transactions in 2024")
     
     figs["fig17"].update_layout(xaxis_type = "category", bargap = 0.2)
-    return figs
+    return figs, state_pie_charts
 
 figs, state_pie_charts = load_business_figures()
 
@@ -1332,6 +1332,7 @@ else:
             - These regions have higher concentration of working professionals, wealthier residents and a strong digital adoption culture fueling rapid insurance uptake through PhonePe.
             - It is also likely that PhonePe actively focused its marketing and outreach efforts in these postal codes, tapping into neighbourhoods known for early tech adoption and openness to digital financial products.
             - Postal codes such as 560103, which corresponds to the Belandur area in Bengaluru, are hubs for IT parks, tech campuses, and newly developed residential complexes, leading to a surge in new residents. As people relocate or find new jobs, insurance purchases, especially health, life or property - often spike as part of onboarding financial planning.""")    
+
 
 
 
